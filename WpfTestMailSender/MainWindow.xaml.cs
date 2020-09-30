1d2b09e2909feca8d921c06da0ddfa457d68fafb
Sergey But <but-sergey@bk.ru>
2020-09-30 21:51:19 +0300

    Пример работы привязок к данным

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -1,43 +1,10 @@
-﻿using System;
-using System.Windows;
-using WpfTestMailServer.Dialogs;
+﻿using System.Windows;
 
 namespace WpfTestMailServer
 {
 
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void btnSendMail_Click(object sender, RoutedEventArgs e)
-        {
-            EmailSendService email = new EmailSendService();
-
-            Settings.SenderName = txtLogin.Text;
-            Settings.SenderPassword = passwordBox.Password;
-            Settings.ToMail = txtTo.Text;
-
-            try
-            {
-                email.SendEmail(txtSubject.Text, txtMailBody.Text);
-
-                SendEndWindow sendEndWindow = new SendEndWindow();
-                sendEndWindow.ShowDialog();
-            }
-            catch (Exception)
-            {
-                SendErrorWindow sendErrorWindow = new SendErrorWindow();
-                sendErrorWindow.ShowDialog();
-            }
-        }
-
-        private void Window_Initialized(object sender, EventArgs e)
-        {
-            txtLogin.Text = Settings.SenderName;
-            passwordBox.Password = Settings.SenderPassword;
-        }
+        public MainWindow() => InitializeComponent();
     }
 }
