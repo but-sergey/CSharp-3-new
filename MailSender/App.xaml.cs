efdb26d66b0e0eea3b6653bd283c2fbdaf93ae63
but-sergey <but-sergey@bk.ru>
2020-10-01 16:41:59 +0300

    Инфраструктура в основном проекте - локатор, хостинг

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using MailSender.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace MailSender
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting
+            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
