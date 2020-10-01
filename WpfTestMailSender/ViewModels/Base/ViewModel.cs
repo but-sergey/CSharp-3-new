9538d39e07e7ee460e3ca582b7345067fe89efad
but-sergey <but-sergey@bk.ru>
2020-10-01 14:05:52 +0300

    Тестовая модель-представление

--- WpfTestMailSender/ViewModels/Base/ViewModel.cs
+++ WpfTestMailSender/ViewModels/Base/ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfTest.ViewModels.Base
 {
@@ -6,9 +7,17 @@ namespace WpfTest.ViewModels.Base
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string PropertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
         }
     }
 }
--- WpfTestMailSender/ViewModels/MainWindowViewModel.cs
+++ WpfTestMailSender/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using WpfTest.ViewModels.Base;
 
 namespace WpfTest.ViewModels
@@ -14,12 +15,32 @@ namespace WpfTest.ViewModels
         public string Title
         {
             get => _Title;
-            set
-            {
-                if (_Title == value) return;
-                _Title = value;
-                OnPropertyChanged("Title");
-            }
+            set => Set(ref _Title, value);
+            //set
+            //{
+            //    if (_Title == value) return;
+            //    _Title = value;
+            //    //OnPropertyChanged("Title");
+            //    //OnPropertyChanged(nameof(Title));
+            //    OnPropertyChanged();
+            //}
+        }
+
+        public DateTime CurrentTime => DateTime.Now;
+
+        private readonly Timer _Timer;
+
+        public MainWindowViewModel()
+        {
+            _Timer = new Timer(100);
+            _Timer.Elapsed += OnTimerElapsed;
+            _Timer.AutoReset = true;
+            _Timer.Enabled = true;
+        }
+
+        private void OnTimerElapsed(object Sender, ElapsedEventArgs E)
+        {
+            OnPropertyChanged(nameof(CurrentTime));
         }
     }
 }
