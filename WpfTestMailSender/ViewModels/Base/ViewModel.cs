8cd5d733e202352f952f162316d0e634af740128
but-sergey <but-sergey@bk.ru>
2020-10-01 13:42:51 +0300

    Реализация модели-представления

---
+++ WpfTestMailSender/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WpfTest.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ WpfTestMailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTest.ViewModels.Base;
+
+namespace WpfTest.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
