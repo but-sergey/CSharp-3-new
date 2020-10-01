97af71c72096189279fd8a1f9cf3b7c15892af04
but-sergey <but-sergey@bk.ru>
2020-10-01 16:18:35 +0300

    Рефакторинг (почистил using-и)

--- MailSender/Infrastructure/Commands/LambdaCommand.cs
+++ MailSender/Infrastructure/Commands/LambdaCommand.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MailSender.Infrastructure.Commands.Base;
+﻿using MailSender.Infrastructure.Commands.Base;
+using System;
 
 namespace MailSender.Infrastructure.Commands
 {
