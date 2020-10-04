dfced3621d5330d82279e60c343149d2bdaa08aa
Sergey But <but-sergey@bk.ru>
2020-10-04 21:43:41 +0300

    Revert "Удалил неиспользуемый обработчик OnSendButtonClick"
    This reverts commit 4eda12c08f3401ec1845e530a0f25de2657108df.

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using MailSender.lib;
+using MailSender.Models;
+using System.Net.Mail;
+using System.Windows;
 
 namespace MailSender
 {
@@ -7,6 +10,41 @@ namespace MailSender
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnSendButtonClick(object Sender, RoutedEventArgs e)
+        {
+            if (!(SendersList.SelectedItem is Sender sender)) return;
+            if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
+            if (!(ServersList.SelectedItem is Server server)) return;
+            if (!(MessagesList.SelectedItem is Message message)) return;
+
+            if(string.IsNullOrWhiteSpace(message.Body))
+            {
+                MessageBox.Show("Письмо не заполнено!", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                MainTabConlrol.SelectedItem = MessagesTab;
+                return;
+            }
+
+            var send_service = new MailSenderService
+            {
+                ServerAddress = server.Address,
+                ServerPort = server.Port,
+                UseSSL = server.UseSSL,
+                Login = server.Login,
+                Password = server.Password
+            };
+
+            try
+            {
+                send_service.SendMessage(sender.Address, recipient.Address, message.Subject, message.Body);
+            }
+            catch (SmtpException error)
+            {
+                MessageBox.Show("Ошибка при отправке почты " + error.Message, "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void OnOpenSchedulerClick(object sender, RoutedEventArgs e)
