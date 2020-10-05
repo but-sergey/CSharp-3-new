c087120282b737d985049d57a27fd3c0414b1019
but-sergey <but-sergey@bk.ru>
2020-10-05 14:27:03 +0300

    Рефакторинг моделей

--- MailSender.lib/Models/Sender.cs
+++ MailSender.lib/Models/Sender.cs
@@ -1,9 +1,6 @@
-﻿namespace MailSender.lib.Models
-{
-    public class Sender
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
+﻿using MailSender.lib.Models.Base;
 
-    }
+namespace MailSender.lib.Models
+{
+    public class Sender : Person { }
 }
