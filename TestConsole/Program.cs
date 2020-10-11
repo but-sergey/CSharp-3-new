b662d0b3fdedc1d936d468a59251671151a8e993
Sergey But <but-sergey@bk.ru>
2020-10-11 17:52:56 +0300

    Простая реализация задач через потоки

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -6,12 +8,82 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            //ThreadTests.Start();
-            //CriticalSectionTests.Start();
-            ThreadPoolTests.Start();
+            //new Thread(() =>
+            //{
+            //    while (true)
+            //    {
+            //        Console.Title = DateTime.Now.ToString();
+            //        Thread.Sleep(100);
+            //    }
+            //})
+            //{ IsBackground = true }.Start();
+
+            //new Task(() =>
+            //{
+            //    while (true)
+            //    {
+            //        Console.WriteLine(DateTime.Now);
+            //        Thread.Sleep(100);
+            //    }
+            //}).Start();
+
+            //var task = new Task(() => { });
+
+            var factorial = new MathTask(() => Factorial(10));
+            var sum = new MathTask(() => Sum(10));
+
+            factorial.Start();
+            sum.Start();
+
+            Console.WriteLine($"Факториал {factorial.Result}, сумма {sum.Result}");
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
         }
+
+        private static int Factorial(int n)
+        {
+            var factorial = 1;
+            for (var i = 1; i <= n; i++)
+                factorial *= n;
+            return factorial;
+        }
+
+        private static int Sum(int n)
+        {
+            var sum = 0;
+            for (var i = 1; i <= n; i++)
+                sum += n;
+            return sum;
+        }
+    }
+
+    class MathTask
+    {
+        private readonly Thread _CalculationThread;
+        private int _Result;
+        private bool _IsCompleted;
+
+        public int Result
+        {
+            get
+            {
+                if (!_IsCompleted)
+                    _CalculationThread.Join();
+                return _Result;
+            }
+        }
+
+        public MathTask(Func<int> Calculation)
+        {
+            _CalculationThread = new Thread(
+                () =>
+                {
+                    _Result = Calculation();
+                    _IsCompleted = true;
+                }) { IsBackground = true };
+        }
+
+        public void Start() => _CalculationThread.Start();
     }
 }
