e587990ad4dbf788788ebe2457b99db9632717e3
Sergey But <but-sergey@bk.ru>
2020-10-11 10:53:30 +0300

    Синхронизация потоков с помощью lock

--- TestConsole/CriticalSectionTests.cs
+++ TestConsole/CriticalSectionTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -11,7 +7,37 @@ namespace TestConsole
     {
         public static void Start()
         {
+            LockSynchronizationTest();
+        }
 
+        private static void LockSynchronizationTest()
+        {
+            var threads = new Thread[10];
+
+            for (var i = 0; i < threads.Length; i++)
+            {
+                var local_i = i;
+                threads[i] = new Thread(() => PrintData($"Message from thread {local_i}", 10));
+            }
+
+            for (var i = 0; i < threads.Length; i++)
+                threads[i].Start();
+        }
+
+        private static readonly object __SyncRoot = new object();
+
+        private static void PrintData(string Message, int Count)
+        {
+            for(var i = 0; i < Count; i++)
+            {
+                lock (__SyncRoot)
+                {
+                    Console.Write($"Thread id:{Thread.CurrentThread.ManagedThreadId}");
+                    Console.Write("\t");
+                    Console.Write(Message);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
