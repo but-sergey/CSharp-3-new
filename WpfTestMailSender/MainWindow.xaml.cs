71ebf8ea3a027ab746594747fe6b66fc0db9f055
Sergey But <but-sergey@bk.ru>
2020-10-11 12:03:21 +0300

    Обращение к элементам пользовательского интерфейса из стороннего потока

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 
 namespace WpfTest
 {
@@ -6,5 +7,23 @@ namespace WpfTest
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void ComputeResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() =>
+            {
+                var result = GetResultHard();
+                Application.Current.Dispatcher.Invoke(() => ResultText.Text = result);
+            }){ IsBackground = true }.Start();
+        }
+
+        private string GetResultHard()
+        {
+            for(var i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+            }
+            return "Hello World";
+        }
     }
 }
