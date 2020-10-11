b518095e2a8463bbe4f36160a8c94c4d340c2a31
Sergey But <but-sergey@bk.ru>
2020-10-11 12:32:01 +0300

    Пулл потоков

--- TestConsole/ThreadPoolTests.cs
+++ TestConsole/ThreadPoolTests.cs
@@ -19,10 +19,18 @@ namespace TestConsole
 
             var timer = Stopwatch.StartNew();
 
+            //ThreadPool.GetAvailableThreads(out var available_worker_threads, out var available_completion_threads);
+            //ThreadPool.GetMinThreads(out var min_worker_threads, out var min_completion_threads);
+            //ThreadPool.GetMaxThreads(out var max_worker_threads, out var max_completion_threads);
+
+            //ThreadPool.SetMinThreads(4, 4);
+            //ThreadPool.SetMaxThreads(16, 16);
+
             for (var i = 0; i < messages.Length; i++)
             {
-                var local_i = i;
-                new Thread(() => ProcessMessage(messages[local_i])) { IsBackground = true }.Start();
+                //var local_i = i;
+                //new Thread(() => ProcessMessage(messages[local_i])) { IsBackground = true }.Start();
+                ThreadPool.QueueUserWorkItem(o => ProcessMessage((string)o),  messages[i]);
             }
 
             timer.Stop();
@@ -33,7 +41,7 @@ namespace TestConsole
         private static void ProcessMessage(string message)
         {
             Console.WriteLine($"Обработка сообщения {message}");
-            Thread.Sleep(5000);
+            //Thread.Sleep(5000);
             Console.WriteLine($"Обработка сообщения {message} закончена");
         }
     }
