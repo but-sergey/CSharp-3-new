fd7e249bcfbb5721cbf36fd5b18131ddf2ba33a4
Sergey But <but-sergey@bk.ru>
2020-10-11 09:59:13 +0300

    Синхронизация и остановка потока

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace TestConsole
@@ -30,8 +31,8 @@ namespace TestConsole
 
             //new Thread(() => PrintMessage(message, count)) { IsBackground = true }.Start();
 
-            var print_tast = new PrintMessageTask(message, count);
-            print_tast.Start();
+            var print_task = new PrintMessageTask(message, count);
+            print_task.Start();
 
             //for(var i = 0; i < 10; i++)
             //{
@@ -39,7 +40,25 @@ namespace TestConsole
             //    Thread.Sleep(10);
             //}
 
+            //var current_process = System.Diagnostics.Process.GetCurrentProcess();
+            //Process.Start("calc.exe");
+
             Console.WriteLine("Главный поток работу закончил!");
+            Console.ReadLine();
+
+            Console.WriteLine("Останавливаю время...");
+
+            timer_thread.Priority = ThreadPriority.BelowNormal;
+
+            __TimerWork = false;
+            if (!timer_thread.Join(100))
+                timer_thread.Interrupt();
+            if (timer_thread.IsAlive)
+                timer_thread.Abort();
+
+            //timer_thread.Abort();
+            //timer_thread.Interrupt();
+
             Console.ReadLine();
         }
 
@@ -55,11 +74,13 @@ namespace TestConsole
             }
         }
 
+        private static bool __TimerWork = true;
+
         private static void TimerMethod()
         {
             PrintThreadInfo();
 
-            while (true)
+            while (__TimerWork)
             {
                 Console.Title = DateTime.Now.ToString("HH:mm:ss.ffff");
                 Thread.Sleep(100);
