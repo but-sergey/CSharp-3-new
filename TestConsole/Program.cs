cd0ab85581b1dc3f714f28cba8f9d28f348537db
Sergey But <but-sergey@bk.ru>
2020-10-12 22:13:42 +0300

    Обзор async/await

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -9,7 +9,14 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            TPLOverview.Start();
+            //TPLOverview.Start();
+
+            var task = AsyncAwaitTest.StartAsync();
+            var process_messages_task = AsyncAwaitTest.ProcessDataTestAsync();
+
+            Console.WriteLine("Тестовая задача запущена и мы её ждём!..");
+
+            Task.WaitAll(task, process_messages_task);
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
